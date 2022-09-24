a2ecf3c6d161d073655337d38773c6b28368ba8e
iwanharyatno <iwnharry61@gmail.com>
2022-09-25 05:11:53 +0700

    Mengganti icon untuk language switcher

--- src/ui/components/language-switcher.js
+++ src/ui/components/language-switcher.js
@@ -41,7 +41,6 @@ customElements.define('language-switcher', class extends HTMLElement {
       #toggler {
         width: 5rem;
         height: 5rem;
-        font-size: 2rem;
         border: none;
         background: transparent;
         user-select: none;
@@ -56,7 +55,11 @@ customElements.define('language-switcher', class extends HTMLElement {
     `;
     this._shadowRoot.innerHTML += `
     <div id="source-lang">${this.sourceLang}</div>
-    <button id="toggler">=&gt;</button>
+    <button id="toggler">
+      <svg xmlns="http://www.w3.org/2000/svg" width="40%" height="40%" fill="currentColor" class="bi bi-arrow-left-right" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"/>
+      </svg>
+    </button>
     <div id="target-lang">${this.targetLang}</div>
     `;
 
